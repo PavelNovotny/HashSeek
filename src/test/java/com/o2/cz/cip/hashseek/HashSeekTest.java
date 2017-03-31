41ac32a7591a0f10537eb66a880775ef65bd70da
Pavel Novotny <pavel.novotny.cz@gmail.com>
2017-03-31 09:16:13 +0200

    test s blocks souborem

--- src/test/java/com/o2/cz/cip/hashseek/HashSeekTest.java
+++ src/test/java/com/o2/cz/cip/hashseek/HashSeekTest.java
@@ -16,9 +16,11 @@ public class HashSeekTest {
     public void testCreateIndex() throws Exception {
         File file = new File("/Users/pavelnovotny/Downloads/transfer/e2e/jms_s1_alsb_aspect.audit.20170209.19");
         File hashFile = new File("/Users/pavelnovotny/Downloads/transfer/e2e/jms_s1_alsb_aspect.audit.20170209.19.hash");
+        File blockFile = new File("/Users/pavelnovotny/Downloads/transfer/e2e/jms_s1_alsb_aspect.audit.20170209.19.blocks");
+        //File blockFile = null;
         HashSeekConstants.outPrintLine("started testCreateIndex");
         BlockHashFileCreator hashCreator = new BlockHashFileCreator();
-        hashCreator.createHashFile(file, hashFile, null);
+        hashCreator.createHashFile(file, hashFile, blockFile);
         HashSeekConstants.outPrintLine("ended testCreateIndex");
     }
 
